1d952e20f12422f5bb6d623fcf148245fe5d4ba8
Michael Flasco <mcflasco@gmail.com>
2016-10-20 10:56:20 -0400

    edited spec files

---
+++ spec/loginSpec.js
@@ -0,0 +1,3 @@
+// Jasmine test for user login information implementation //
+
+// This will check if nothing is inputed and will test is user has implemented username and password //
---
+++ spec/userSpec.js
@@ -0,0 +1,9 @@
+// Jasmine testing for inputting username information (name, email, password) //
+
+
+// this will check that user is inputing the required information into the username create form //
+
+describe('check user information'function(){
+	it('')
+})
+
