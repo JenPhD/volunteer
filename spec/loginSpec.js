a7912bb52785738ad0ef01ba3e40a3cff3e634b8
Michael Flasco <mcflasco@gmail.com>
2016-10-20 22:44:17 -0400

    loginSpec, userSpec

--- spec/loginSpec.js
+++ spec/loginSpec.js
@@ -1,3 +1,30 @@
-// Jasmine test for user login information implementation //
+// Jasmine unit testing to determine the login parameters, submit function, and error message is working properly //
 
-// This will check if nothing is inputed and will test is user has implemented username and password //
+
+describe('login', function(){
+	
+	// var login = require("......");
+
+	// it('login button switches to login information section', function(){
+	// 	expect(login)
+	// });
+
+// not sure if we will need these but i put them in just incase //
+	it('check if username was entered in login', function(){
+		expect(login.username).tobe(true);
+	});
+// not sure if we will need these but i put them in just incase //
+	it('check if password was entered in login section',function(){
+		expect(login.Password).tobe(true);
+	});
+
+	it('error message displays when information has been submitted incorrectly or left blank', function(){
+		expect(login.error).tothrowError('Please enter the required information.');
+		expect(login.error).tothrowError(typeError);
+	});
+
+		it('sumbit function of username form', function(){
+		expect(login).toequal(login);
+	});
+
+});
