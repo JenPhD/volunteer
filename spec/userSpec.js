0008f193d0c8b95f3f8b6d316963e2d3c8fce686
Michael Flasco <mcflasco@gmail.com>
2016-10-20 21:56:59 -0400

    userSpec.js

--- spec/userSpec.js
+++ spec/userSpec.js
@@ -1,9 +1,34 @@
-// Jasmine testing for inputting username information (name, email, password) //
+// Jasmine testing for inputting username information to create a user name and password (name, email, password) //
 
 
 // this will check that user is inputing the required information into the username create form //
 
-describe('check user information'function(){
-	it('')
-})
+
+describe('create username and password', function(){
+
+// not sure if we will need these but i put them in just incase //
+	it('check is name has been entered', function(){
+		expect(name).tobe(true);	
+	});
+// not sure if we will need these but i put them in just incase //
+	it('check if username was entered', function(){
+		expect(userName).tobe(true);
+	});
+// not sure if we will need these but i put them in just incase //
+	it('check if password was entered',function(){
+		expect(Password).tobe(true);
+	});
+
+	it('error message displays when information has been submitted incorrectly or left blank', function(){
+		expect(error).tothrowError('Please enter the required information.');
+		expect(error).tothrowError('You must fill out the required fields.');
+		expect(error).tothrowError(typeError);
+	});
+
+	it('sumbit function of username form', function(){
+		expect(submit).tobe()
+	});
+
+
+});
 
